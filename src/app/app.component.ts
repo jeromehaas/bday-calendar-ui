da952df19c58ffaaa40c1cbed9bedb36e7e4c3df
jeromehaas <hello@jeromehaas.ch>
2024-07-01 14:31:12 +0200

    Setup: routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,6 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
 })
 
 // EXPORTS
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,20 @@
 // IMPORTS
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {OverviewComponent} from "./pages/overview/overview.component";
 
 // EXPORTS
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'overview',
+        pathMatch: 'full',
+    },
+    {
+        path: 'overview',
+        component: OverviewComponent,
+    },
+    {
+        path: '**',
+        redirectTo: '/overview',
+    }
+];
