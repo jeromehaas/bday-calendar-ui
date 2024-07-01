e9445977998c416000891e0974634252ce1f5a37
jeromehaas <hello@jeromehaas.ch>
2024-07-01 14:45:48 +0200

    Setup: site layout

---
+++ src/app/components/layouts/site/site.component.ts
@@ -0,0 +1,14 @@
+// IMPORTS
+import {Component} from '@angular/core';
+
+// COMPONENT
+@Component({
+    selector: 'app-site',
+    standalone: true,
+    imports: [],
+    templateUrl: './site.component.html',
+    styleUrl: './site.component.scss'
+})
+
+// SITE COMPONENT
+export class SiteComponent {}
